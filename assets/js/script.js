e3d36a3490dd059b49d0d10e5db8f1cc1fea770d
dvnnorth <13654540+dvnnorth@users.noreply.github.com>
2018-06-22 21:01:54 -0500

    Got axios call functioning properly with CORS browser addon

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,24 @@ $(function () {
     // Initialize the sidenav bar
     $('.sidenav').sidenav();
 
+    let map;
+    function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: { lat: 29.7604, lng: -95.3698 },
+            zoom: 8
+        });
+    }
+
+    initMap();
+
+    let lyftEndpoint = "https://api.lyft.com/v1/cost?start_lat=37.7763&start_lng=-122.3918&end_lat=37.7972&end_lng=-122.4533";
+
+    axios.get(lyftEndpoint)
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
 });
